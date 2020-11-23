bcbca8513b00c854943193297c1410ec9925a7d3
Wolodimir <dvdnent666@gmail.com>
2020-11-23 12:24:43 +0400

    Maven init

---
+++ src/test/java/com/vladcom/capitalism/CapitalismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladcom.capitalism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitalismApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
