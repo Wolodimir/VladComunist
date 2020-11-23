de4222e0652b50637cd3296e05f83dbbb6628d78
Wolodimir <dvdnent666@gmail.com>
2020-11-23 12:48:45 +0400

    Vlads interface added, and everything works!

---
+++ src/main/java/com/vladcom/capitalism/controllers/indexController.java
@@ -0,0 +1,42 @@
+package com.vladcom.capitalism.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class indexController {
+    @GetMapping("/Cap")
+    public String indexPage(Model model){
+        model.addAttribute("tak", "ДОБРОПОЖАЛОВАТЬ");
+        return "index";
+    }
+
+    @PostMapping("/Cap")
+    public String capitalCalc(@RequestParam String capital, Model model){
+        if(capital.equals("")){
+            capital = "0";
+        }
+        String verdict = "ВВЕДИ СВОЙ КАПИТАЛ ДРУЖОК!";
+        int cap = Integer.parseInt(capital);
+        if(cap <= 1000 && cap > 0){
+            verdict = "ТЫ НАШ! ПРОЛЕТАРИАТ!";
+        }
+        else if(cap > 1000){
+            verdict = "РАСКУЛАЧИТЬ";
+        }
+        else if(cap == 0){
+            verdict = "ИДИ РАБОТАЙ! ТУНЕЯДЕЦ!";
+        }
+        model.addAttribute("tak", verdict);
+        return "index";
+    }
+
+    @GetMapping("/second")
+    public String secondPage(){
+        return "second";
+    }
+}
+
